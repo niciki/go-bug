7dd7fcbf17d1b4f4f0b42de00e18b28f619050ac
Nikita <71276784+niciki@users.noreply.github.com>
2023-02-27 17:09:03 +0300

    Delete bug directory

--- bug/internal/interfaces/tg.go
+++
@@ -1,6 +0,0 @@
-// @tg packageJSON=`github.com/seniorGolang/json`
-//
-//go:generate tg transport --services . --out ../transport --outSwagger ../../api/swagger.yaml
-//go:generate tg client -go --services . --outPath ../clients/users
-
-package interfaces
